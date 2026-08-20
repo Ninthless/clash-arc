306ccfa71101eb161d17d1366348c7972b02e4fa
c9 <3124495484@qq.com>
2026-08-20 23:37:12 +0800

    fix(ci): support current packaging toolchain

--- setup.dart
+++ setup.dart
@@ -147,11 +147,6 @@ Future<int> _package(
     platform: platform,
     verbose: verbose,
   );
-  final descriptionArgs = <String>[];
-  if (platform != 'android') {
-    descriptionArgs.addAll(['--description', arch]);
-  }
-
   final depExit = await _ensureDependencies(platform, arch);
   if (depExit != 0) return depExit;
 
@@ -185,7 +180,6 @@ Future<int> _package(
         '--build-target-platform=${_androidFlutterTarget[androidArch]!}',
       if (flutterBuildArgs.isNotEmpty)
         '--flutter-build-args=${flutterBuildArgs.join(',')}',
-      ...descriptionArgs,
     ],
     includeParentEnvironment: true,
     environment: {'ANDROID_ARCH': ?androidArch},
